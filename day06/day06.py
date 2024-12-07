f30b1493ac06ab2aa9049c9c98dcbb8853a6b0d5
opitt <63d64c2bc2b1cb6b346f0a1d@bots.bitbucket.org>
2024-12-07 22:37:20 +0100

    adjusted part 1 but part 2 not working (day 6)

--- day06/day06.py
+++ day06/day06.py
@@ -1,4 +1,5 @@
 # https://adventofcode.com/2024/day/6
+from copy import deepcopy
 import os
 import itertools as it
 
@@ -6,7 +7,7 @@ import itertools as it
 def solve1(maze):
     max_x = len(maze[0])
     max_y = len(maze)
-    pos_x, pos = y = 0, 0
+
     for y, line in enumerate(maze):
         try:
             x = line.index("^")
@@ -16,23 +17,78 @@ def solve1(maze):
         except:
             pass
 
-    DIRS = it.cycle([(-1, 0), (0, 1), (1, 0), (0, -1)])
-    dy,dx = next(DIRS)
+    visited = []
+
+    DIR = {"^": (-1, 0), ">": (0, 1), "v": (1, 0), "<": (0, -1)}
+    DIRS = it.cycle("^>v<")
+    dir = next(DIRS)
+    dy, dx = DIR[dir]
     while True:
-        maze[pos_y][pos_x] = "X"
-        if pos_y+dy < 0 or pos_y+dy >= max_y or  pos_x+dx < 0 or pos_x+dx >= max_x:
+        visited.append((pos_y, pos_x, dir))
+        if (
+            pos_y + dy < 0
+            or pos_y + dy >= max_y
+            or pos_x + dx < 0
+            or pos_x + dx >= max_x
+        ):
             break
-        if maze[pos_y+dy][pos_x+dx]=="#":
-            dy,dx = next(DIRS)
-        pos_y+=dy
-        pos_x+=dx
-    res=sum(line.count("X") for line in maze)
+        if maze[pos_y + dy][pos_x + dx] == "#":
+            dir = next(DIRS)
+            visited.append((pos_y, pos_x, dir))
+            dy, dx = DIR[dir]
+        pos_y += dy
+        pos_x += dx
+    res = len(set((y, x) for y, x, _ in visited))
     return res
 
 
 def solve2(maze):
+    max_x = len(maze[0])
+    max_y = len(maze)
+    res=0
+    for y, line in enumerate(maze):
+        try:
+            x = line.index("^")
+            pos_y = y
+            pos_x = x
+            break
+        except:
+            pass
 
-    res = 0
+    visited = []
+
+    DIR = {"^":(-1, 0), ">":(0, 1), "v":(1, 0), "<":(0, -1)}
+    NEXT_DIR = {"^":">",">":"v","v":"<","<":"^"}
+
+    dir = "^"
+    dy,dx=DIR[dir]
+    while True:
+        visited.append((pos_y,pos_x,dir))
+        if (
+            pos_y + dy < 0
+            or pos_y + dy >= max_y
+            or pos_x + dx < 0
+            or pos_x + dx >= max_x
+        ):
+            break
+
+        if maze[pos_y + dy][pos_x + dx] == "#":
+            dir = NEXT_DIR[dir]
+            visited.append((pos_y,pos_x,dir))
+            dy,dx=DIR[dir]
+        else:
+            what_if_dir = NEXT_DIR[dir]
+            what_if_dy, what_if_dx = DIR[what_if_dir]
+            if (
+                pos_y + what_if_dy,
+                pos_x + what_if_dx,
+                what_if_dir,
+            ) in visited:  # loop opportunity
+                res += 1
+
+        pos_y += dy
+        pos_x += dx
+
     return res
 
 
@@ -45,8 +101,8 @@ def main(test):
         lines = input.readlines()
     lines = list(map(list, list(map(str.strip, lines))))
 
-    result1 = solve1(lines)
-    result2 = solve2(lines)
+    result1 = solve1(deepcopy(lines))
+    result2 = solve2(deepcopy(lines))
     print(
         f"The result for part 1 is {result1}",
         f"The result for part 2 is {result2}",
